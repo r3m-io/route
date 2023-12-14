eaff85511fe42f349567bb904d56b8eb161dbad7
remco van der Velde <remco.pc@outlook.com>
2023-12-14 21:21:35 +0100

    Remove autoload configuration from Cli.php
    The modifications removed the autoload configuration setup lines from 'Cli.php'. This includes removing the prefix, directory, and autoload properties from the 'Data' object. The simplification prepares the file for future modifications and follows the trend toward removing unnecessary initializations. It is expected to ease understanding of the run() function.

--- src/Package/Controller/Cli.php
+++ src/Package/Controller/Cli.php
@@ -31,14 +31,6 @@ class Cli extends Controller {
      * @throws Exception
      */
     public static function run(App $object){
-        $autoload = [];
-        $data = new Data();
-        $data->set('prefix', 'Node');
-        $data->set('directory', $object->config('project.dir.node'));
-        $autoload[] = clone $data->data();
-        $data->clear();
-        $data->set('autoload', $autoload);
-        Cli::autoload($object, $data);
         $node = $object->request(0);
         $scan = Cli::scan($object);
         $module = $object->parameter($object, $node, 1);
